47a4c07ce1772f38cd9cfe6ee9372dc6fc7eb94f
Trent Shailer <trent.shailer@gmail.com>
2022-06-04 13:33:59 +1200

    Basic block stacking

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
 	private static GameManager instance;
 	public GameObject blockPrefab;
+	public GameObject blockParent;
 
 	public static GameManager GetInstance(){
 		return instance;
@@ -29,7 +31,18 @@ public class GameManager : MonoBehaviour
 	public void AddBlock(BlockInstance block)
 	{
 		ownedBlocks.Add(block);
-		Instantiate(blockPrefab);
+
+		GameObject blockObj = Instantiate(blockPrefab, blockParent.transform);
+
+		blockObj.transform.localPosition = new Vector3(0, ownedBlocks.Count, 0);
+		blockObj.transform.Find("Name").GetComponent<TextMeshPro>().text = block.name;
+
+		if(ownedBlocks.Count > 7)
+		{
+			Vector3 prevPos = blockParent.transform.localPosition;
+			blockParent.transform.localPosition = new Vector3(prevPos.x, prevPos.y - 1, prevPos.z);
+		}
+
 		for(int i = 0; i < ownedBlocks.Count; i++)
         {
 			portfolioValue += (ownedBlocks[i].profit);
