359fa2bd70be6ab6c4c4652e2a15ff1f0c4456cf
DESKTOP-71JM52U\Samuel <ngsa7956@student.otago.ac.nz>
2022-07-02 13:13:03 +1200

    Converted EventGenerator to a singleton. Assigned multiple values to be utilized by GameManager. Added in Events in GameManager.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,13 +14,21 @@ public class GameManager : MonoBehaviour
 	}
 
 	public float stability = 0;
-	public int portfolioValue = 0;
+	public float portfolioValue = 0;
 	public int profits = 0;
 	public int towerHeight = 0;
 
 	//Chance for event to occur
-	public float eventchance = 0f;
-	public int eventduration = 0;
+	public float eventChance = 0f;
+
+	//How long the event will be for.
+	public int eventDuration = 0;
+
+	//Whether there is an event ongoing
+	public bool eventOccuring = false;
+
+	//Whether the stability has already been multiplied 
+	public bool stabMultiplied = false;
 
 	public List<BlockInstance> ownedBlocks = new List<BlockInstance>();
 
@@ -28,21 +36,88 @@ public class GameManager : MonoBehaviour
 	{
 		ownedBlocks.Add(block);
 
-
-
-		//Any time a block is added, the chance for an event increases.
-		eventchance += 5;
+		//Only if there is no event happening.
+		if (!eventOccuring)
+		{
+			//Any time a block is added, the chance for an event increases.
+			eventChance += 5;
+		}
 		//If the generated number is less than or equal to event chance, then generate an event.
-		if (Random.Range(0, 100) <= eventchance)
+		if (Random.Range(0, 100) <= eventChance)
 		{
 			//Randomly picks from one of the 5 events available in EventType.
-			EventGenerator.GenerateEvent((EventType)Random.Range(0, 5));
+			EventGenerator.Instance.GenerateEvent((EventType)Random.Range(0, 5));
+			//Sets event occuring to true.
+			eventOccuring = true;
+			//Resets event chance.
+			eventChance = 0f;
+			eventDuration = EventGenerator.Instance.selectRounds;
 		}
 		TowerAnimator.Instance.AddBlockToTower(block);
 
 		for (int i = 0; i < ownedBlocks.Count; i++)
 		{
-			portfolioValue += (ownedBlocks[i].profit);
+			//Change profit by multiplier from Generated Event.
+			float multiplier = 1f;
+            if (eventOccuring)
+            {
+				if (EventGenerator.Instance.selectIndex == 1)
+				{
+					//If the block matches the record, or it affects all blocks
+					if (EventGenerator.Instance.blockRecord == ownedBlocks[i].blockType || EventGenerator.Instance.blockIndex == 0)
+					{
+						//If the event is a multiplicative/divisive event, set the multiplier accordingly.
+						if (EventGenerator.Instance.eventRecord == EventType.Fractional || EventGenerator.Instance.eventRecord == EventType.Multiplier)
+						{
+							multiplier = EventGenerator.Instance.selectMult;
+						}
+
+						//If the event is to nullify a value, then remove it.
+						if (EventGenerator.Instance.eventRecord == EventType.BlockNullification)
+						{
+							multiplier = 0;
+						}
+
+						//If the event is to add a block
+						if (EventGenerator.Instance.eventRecord == EventType.BlockAddition)
+						{
+							//Add a random block here.
+							eventOccuring = false;
+							eventDuration = 0;
+						}
+
+						//If the event is to remove a block
+						if (EventGenerator.Instance.eventRecord == EventType.BlockRemoval)
+						{
+							//Add a random block here.
+							eventOccuring = false;
+							eventDuration = 0;
+						}
+					}
+				}
+				//Stability portion is here.
+				else
+				{
+					if (EventGenerator.Instance.blockRecord == ownedBlocks[i].blockType || EventGenerator.Instance.blockIndex == 0)
+					{
+						//If the event is a multiplicative/divisive event, set the multiplier accordingly.
+						if (EventGenerator.Instance.eventRecord == EventType.Fractional || EventGenerator.Instance.eventRecord == EventType.Multiplier)
+						{
+							if (!stabMultiplied)
+							{
+								ownedBlocks[i].stability = ownedBlocks[i].stability * EventGenerator.Instance.selectMult;
+							}
+						}
+					}
+				}
+			}
+
+			//If the event occuring is a stability event, then set the multiplication value to true.
+			if (EventGenerator.Instance.selectIndex == 0)
+			{
+				stabMultiplied = true;
+			}
+			portfolioValue += (ownedBlocks[i].profit) * multiplier;
 		}
 
 		if (stability < -1)
@@ -55,10 +130,37 @@ public class GameManager : MonoBehaviour
 			stability = 1;
 		}
 
-		//If there is an ongoing event, count down.
-		if (eventduration > 0)
+		//If there is an ongoing event
+		if (eventOccuring)
 		{
-			eventduration--;
+			//If the event is 0, set the event occuring to false.
+			if (eventDuration == 0)
+			{
+				//If the event was a stability event, reset the values.
+				if(stabMultiplied)
+                {
+					for (int i = 0; i < ownedBlocks.Count; i++)
+					{
+						if (EventGenerator.Instance.blockRecord == ownedBlocks[i].blockType || EventGenerator.Instance.blockIndex == 0)
+						{
+							//If the event is a multiplicative/divisive event, revert the multiplier accordingly.
+							if (EventGenerator.Instance.eventRecord == EventType.Fractional || EventGenerator.Instance.eventRecord == EventType.Multiplier)
+							{
+								ownedBlocks[i].stability = ownedBlocks[i].stability / EventGenerator.Instance.selectMult;
+							}
+						}
+					}
+					stabMultiplied = false;
+				}
+
+				eventOccuring = false;
+			}
+			else
+			{
+				//If not, reduce duration by 1, since we added a block.
+				eventDuration--;
+			}
+			
 		}
 	}
 
--- Assets/Scripts/GameScripts/EventGenerator.cs
+++ Assets/Scripts/GameScripts/EventGenerator.cs
@@ -4,44 +4,79 @@ using UnityEngine;
 
 public class EventGenerator : MonoBehaviour
 {
-    public static int[] rounds = new int[] { 1, 2, 5 };
-    public static int[] multiplier = new int[] { 2, 5, 10 };
-    public static string[] selector = new string[] { "stability", "profit" };
-    public static string[] blockSelector = new string[] { "all blocks", "all blocks of a random type", "all blocks in a random group" };
+    //Create a singleton for EventGenerator
+    public static EventGenerator Instance { get; private set; }
 
+    void Awake()
+    {
+        if (!Instance) Instance = this;
+    }
+
+    public string[] selector = new string[] { "stability", "profit" };
+    public int[] multiplier = new int[] { 2, 5, 10 };
+    public int[] rounds = new int[] { 1, 2, 5 };
+    public string[] blockSelector = new string[] { "", " of the Insurance Type", " of the Low Risk Type", " of the High Risk Type" };
+  
 
     //Moved variables to public static so that GameManager can refer to them for newly added blocks.
-    public static string selectType;
-    public static int selectMult;
-    public static int selectRounds;
-    public static string selectBlocks;
-    public static EventType eventRecord;
+    public string selectType;
+    public int selectMult;
+    public int selectRounds;
+    public string selectBlocks;
+    public EventType eventRecord;
+    public BlockType blockRecord;
+    public int blockIndex;
+    public int selectIndex;
 
     //Not sure whether I want to use a dictionary for the event.
     private static readonly Dictionary<EventType, string> eventList = new()
     {
-        { EventType.Multiplier, "Multiply {0} by {1} for the next {2} rounds for {3}" },
-        { EventType.Fractional, "Divide {0} by {1} for the next {2} rounds for {3}" },
+        { EventType.Multiplier, "Multiply {0} by {1} for the next {2} rounds for all blocks{3}" },
+        { EventType.Fractional, "Divide {0} by {1} for the next {2} rounds for all blocks{3}" },
         { EventType.BlockRemoval, "Removes a random block from the tower" },
         { EventType.BlockAddition, "Adds a random block to the tower"},
-        { EventType.BlockNullification, "{3} no longer generate profit for a single round."}
+        { EventType.BlockNullification, "all blocks{3} no longer generate profit for a single round."}
     };
 
     //Where all altering factors should be for ownedblocks.
-    public static string GenerateEvent(EventType eventType)
+    public string GenerateEvent(EventType eventType)
     {
-        //If there is an ongoing event, do not do anything. This depends on whether they want stacking events or not - If they do, this code is not needed.
-        if (GameManager.Instance.eventduration > 0)
+        //If there is an ongoing event, do not do anything. This code SHOULD NOT OCCUR, as Generate Event should only be called when eventoccuring is false.
+        if (GameManager.Instance.eventOccuring)
         {
             return "";
         }
+        //Sets indexes for values GameManager uses
+        blockIndex = Random.Range(0, blockSelector.Length);
+        selectIndex = Random.Range(0, selector.Length);
 
-        //For altering parameters.
-        selectType = selector[Random.Range(0, selector.Length)];
-        selectMult = multiplier[Random.Range(0, multiplier.Length)];
+        //Selects accordingly.
+        selectType = selector[selectIndex];
         selectRounds = rounds[Random.Range(0, rounds.Length)];
-        selectBlocks = blockSelector[Random.Range(0, blockSelector.Length)];
+        selectBlocks = blockSelector[blockIndex];
         eventRecord = eventType;
+
+
+        if (eventType == EventType.Fractional)
+        {
+            selectMult = 1/multiplier[Random.Range(0, multiplier.Length)];
+        } else
+        {
+            selectMult = multiplier[Random.Range(0, multiplier.Length)];
+        }
+
+        if (blockIndex == 1)
+        {
+            blockRecord = BlockType.Insurance;
+        }
+        if (blockIndex == 2)
+        {
+            blockRecord = BlockType.LowRiskInvestment;
+        }
+        if (blockIndex == 3)
+        {
+            blockRecord = BlockType.HighRiskInvestment;
+        }
 
         //String that is generated. 
         string printList = string.Format(eventList[eventType], selectType, selectMult, selectRounds, selectBlocks).ToUpper();
