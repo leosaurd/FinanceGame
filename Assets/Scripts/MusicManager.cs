b7ce8d418841a0c0b33a68ad861dd06e22880d04
DESKTOP-71JM52U\Samuel <ngsa7956@student.otago.ac.nz>
2022-08-13 11:19:02 +1200

    Added in the music.

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public static MusicManager Instance { get; private set; }
+
+    public AudioSource audioSource;
+    private float volume = 1;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            audioSource = GetComponent<AudioSource>();
+        }
+    }
+
+        // Update is called once per frame
+        void FixedUpdate()
+    {
+        if (SettingsManager.Instance.music)
+        {
+            audioSource.volume = volume;
+        }
+        else
+        {
+            audioSource.volume = 0f;
+        }
+    }
+}
