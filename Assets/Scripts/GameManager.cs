cfe6c50c8660af962edd6ef02b4f695029ce6927
Trent Shailer <trent.shailer@gmail.com>
2022-08-25 16:15:35 +1200

    Fix marketplace addition/removal event bug

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -128,6 +128,7 @@ public class GameManager : MonoBehaviour
 					}
 
 					EventUIManager.Instance.blockObject.gameObject.SetActive(false);
+					MarketplaceUI.Instance.RefreshShop();
 				});
 			}
 			else if (gameEvent is LastingEvent)
